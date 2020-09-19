e20334a995b55e2d7703ea2227ea1f026477344d
mehdibenfeguir <mehdi.benfeguir@gmail.com>
2020-09-19 17:07:11 +0300

    Update owasp-dependency-check.sh

--- owasp-dependency-check.sh
+++ owasp-dependency-check.sh
@@ -16,7 +16,7 @@
         fi
         
         # Make sure we are using the latest version
-        
+        apt-get install docker
          docker pull owasp/dependency-check:$DC_VERSION
         
          docker run --rm \
